c6094f184322829c1243c2f4393924cfb1f64f10
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-09-19 10:05:07 +0200

    doc

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ with open('rdu-weather-history.json', 'r') as json_file:
 
 @app.get('/date')
 async def afficher_date():
-    """Cette fonction permet d'afficher toutes les dates présentes dans le fichier json."""
+    """
+    Cette fonction permet d'afficher toutes les dates présentes dans le fichier json.
+    """
     return weather_data
 
 @app.get('/date/{id}')
 async def filtre_date(id : int):
-    """Cette fonction permet de filtrer les données par date."""
+    """
+    Cette fonction permet de filtrer les données par date.
+    Args:
+        params : id de recherche.
+    Returns:
+        la date en fonction de l'id.
+    """
     weather_data_id = weather_data[id]
     return weather_data_id
+
 
 if __name__ == '__main__':
     import uvicorn
