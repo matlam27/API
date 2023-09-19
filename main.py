0654853b67a2714c1472bc0edb63afa7d6456de9
Sacha <sacha.harel@gmail.com>
2023-09-19 09:58:32 +0200

    update main.py and rdu-weather-history.json

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ with open('rdu-weather-history.json', 'r') as json_file:
 
 @app.get('/date')
 async def afficher_date():
-    """Cette fonction permet d'afficher toutes les dates présentes dans le fichier json."""
     return weather_data
+
+@app.get('/filter/{annee}-{mois}-{jour}')
+async def filtrer_date(annee, mois, jour):
+    """
+    Cette fonction permet d'afficher les données d'une date précise.
+    """
+    date = (f'{annee}-{mois}-{jour}')
+    for i in weather_data:
+        if i['date'] == date:
+            return i
 
 if __name__ == '__main__':
     import uvicorn
