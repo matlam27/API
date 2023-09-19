01cc3696a09b1ab421d7fd8b0cf9cf5a2778e65c
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-09-19 09:43:14 +0200

    fonction de filtre et .idea dans le gitignore

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ async def afficher_date():
     """Cette fonction permet d'afficher toutes les dates présentes dans le fichier json."""
     return weather_data
 
+@app.get('/date/{id}')
+async def filtre_date(id : int):
+    """Cette fonction permet de filtrer les données par date."""
+    weather_data_id = weather_data[id]
+    return weather_data_id
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
