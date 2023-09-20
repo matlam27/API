752256432917599b45fee7fcf734700657c95b0e
Sacha <sacha.harel@gmail.com>
2023-09-20 11:52:08 +0200

    update .gitignore, main.py and mysql_connection.py

--- mysql_connection.py
+++ mysql_connection.py
@@ -4,22 +4,10 @@ config = {
   'user': 'root',
   'password': 'root',
   'host': '127.0.0.1',
-  'port': 8889,
+  'port': 8000,
   'database': 'test',
   'raise_on_warnings': True
 }
 
-cnx = mysql.connector.connect(**config)
-
-cursor = cnx.cursor(dictionary=True)
-
-cursor.execute('SELECT `id`, `name` FROM `test`')
-
-results = cursor.fetchall()
-
-for row in results:
-  id = row['id']
-  title = row['name']
-  print '%s | %s' % (id, title)
-
-cnx.close()
+def get_database_connection():
+  return mysql.connector.connect(**config)
