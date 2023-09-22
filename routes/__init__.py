61eef34c1cab7087a4a6c4df85a31f6362b97bb5
Sacha <sacha.harel@gmail.com>
2023-09-22 11:32:50 +0200

    update routes/__init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from . import afficher_donnees, ajouter_date, filtre_date, filtre_tmax, filtre_tmin, precipitation_date, supprimer_date, update, filter_country, filter_city, ajouter_city, ajouter_country, afficher_country, afficher_city
+from . import afficher_meteo, ajouter_date, filtre_date, filtre_tmax, filtre_tmin, precipitation_date, supprimer_date, update, filter_country, filter_city, ajouter_city, ajouter_country, afficher_country, afficher_city
 
 router = APIRouter()
 
@@ -9,7 +9,7 @@ router.include_router(ajouter_city.router, prefix="/city")
 router.include_router(ajouter_country.router, prefix="/country")
 
 # GET methods
-router.include_router(afficher_donnees.router)
+router.include_router(afficher_meteo.router)
 router.include_router(afficher_city.router, prefix="/city")
 router.include_router(afficher_country.router, prefix="/country")
 router.include_router(filtre_date.router, prefix="/weather")
@@ -18,13 +18,6 @@ router.include_router(filtre_tmin.router, prefix="/tmin")
 router.include_router(precipitation_date.router, prefix="/prcp")
 router.include_router(filter_city.router, prefix="/city")
 router.include_router(filter_country.router, prefix="/country")
-
-# DELETE methods
-router.include_router(supprimer_date.router, prefix="/weather")
-
-# PATCH methods
-router.include_router(update.router, prefix="/weather")
-
 
 # DELETE methods
 router.include_router(supprimer_date.router, prefix="/weather")
